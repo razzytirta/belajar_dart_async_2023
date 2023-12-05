047ee9a6409280baaf5689d88c845eda4216d3e4
razzytirta <azzi.fachrurazzi@gmail.com>
2023-12-05 16:01:28 +0800

    Stream Subscription

--- bin/05_stream.dart
+++ bin/05_stream.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 Stream<String> stream() {
   return Stream.periodic(Duration(seconds: 2), (i) {
     if (i % 2 == 0) {
@@ -9,6 +11,18 @@ Stream<String> stream() {
 }
 
 void main() {
-  stream();
+  Stream<String> flow = stream();
+  StreamSubscription<String> listen = flow.listen((event) {
+    print(event);
+  });
+
+  listen.onData((data) {
+    print('Stream Subscription : $data');
+  });
+
+  listen.onDone(() {
+    print('Stream Subscription done');
+  });
+
   print('All Done');
 }
